179b7af7932c4768cfc5469694170d3eafc0b9f3
tomascieska <tomas.cieska@gmail.com>
2024-01-20 11:03:45 +0000

    initial commit

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,18 +1,36 @@
 const express = require('express')
 const route = express.Router()
+const Subscriber = require('../modules/subscriber')
 
 // Get all
-route.get('/', (req, res) => {
-    res.send('Hello')
+route.get('/', async (req, res) => {
+
+    try {
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    } catch (err){
+        res.status(500).json({message: err.message})
+    }
 })
+
 // Get one
 route.get('/:id', (req, res) => {
-
+    res.send(req.params.id)
 })
 // Create one
-route.post('/', (req, res) => {
-
+route.post('/', async (req, res) => {
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        subscribedToChannel: req.body.subscribedToChannel
+    })
+    try {
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    } catch (err) {
+        res.status(400).json({message: err.message})
+    }
 })
+
 // Update one
 route.patch('/:id', (req, res) => {
 
