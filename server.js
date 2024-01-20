3d1be17d2eccf1182a099572c57e3d0f648278a6
tomascieska <tomas.cieska@gmail.com>
2024-01-20 18:42:51 +0000

    initial commit

--- server.js
+++ server.js
@@ -13,29 +13,6 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to database'))
 
-const weatherAIScheama = new mongoose.Schema({
-    town: {
-        type: String,
-        required: true
-    },
-    temperature: {
-        type: Number,
-        required: true
-    }
-})
-
-const collection = new mongoose.model('weather', weatherAIScheama)
-
-const data = {
-    town: "Paris",
-    temperature: 20
-}
-
-setInterval(() => {
-    collection.insertMany(data)
-}, 5000)
-
-
 // Excepts JSON 
 app.use(express.json())
 
